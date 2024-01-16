95e24a404791a0efd0473f53a8925522ccb48a1b
Tian_que <1605206150@qq.com>
2024-01-16 23:09:31 +0800

    done with lab2

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,7 +10,7 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        while (head.rest != null) {
+        while (head != null) {
             head.first += c;
             head = head.rest;
         }
@@ -54,6 +54,9 @@ public class IntListExercises {
         while (x > 10) {
             x = x / 10;
         }
+        if (x == 10) {
+            return 1 == lastDigit;
+        }
         int firstDigit = x % 10;
         return firstDigit == lastDigit;
     }
@@ -77,6 +80,6 @@ public class IntListExercises {
             lst.first *= lst.first;
         }
 
-        return currElemIsPrime || squarePrimes(lst.rest);
+        return squarePrimes(lst.rest) || currElemIsPrime;
     }
 }
--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -16,5 +16,10 @@ public class SquarePrimesTest {
         boolean changed = IntListExercises.squarePrimes(lst);
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
+
+        IntList lst2 = IntList.of(1, 3, 5, 7, 9);
+        boolean changed2 = IntListExercises.squarePrimes(lst2);
+        assertEquals("1 -> 9 -> 25 -> 49 -> 9", lst2.toString());
+        assertTrue(changed2);
     }
 }
