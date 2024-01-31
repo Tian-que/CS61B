8e8e32aac472df3081dfe38ca02eca49b9850093
Tian_que <1605206150@qq.com>
2024-01-31 19:24:43 +0800

    done deque2

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,2 +1,29 @@
-package deque;public class MaxArrayDeque {
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T>{
+
+    private Comparator<T> comparator;
+
+    public MaxArrayDeque(Comparator<T> c) {
+        comparator = c;
+    }
+
+    public T max() {
+        return max(comparator);
+    }
+
+    public T max(Comparator<T> c) {
+        if (isEmpty()) {
+            return null;
+        }
+        T m = get(0);
+        for (int i = 0; i < size(); i++) {
+            if (c.compare(m ,get(i)) > 0){
+                m = get(i);
+            }
+        }
+        return m;
+    }
 }
